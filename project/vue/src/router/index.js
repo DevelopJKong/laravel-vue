2390f34d5e2044363fe3c9134cc91e281b86a99d
DevelopJKong <ybaram40@gmail.com>
2022-05-16 22:16:34 +0900

    vue routes 제작중 [31:22 coding...]

--- project/vue/src/router/index.js
+++ project/vue/src/router/index.js
@@ -2,31 +2,30 @@ import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
-
-
+import DefaultLayout from "../components/DefaultLayout.vue";
 
 const routes = [
     {
-        path:"/",
-        name:"Dashboard",
-        component: Dashboard
+        path: "/",
+        redirect: "/dashboard",
+        name: "Dashboard",
+        component: DefaultLayout,
     },
     {
-        path:"/login",
-        name:"Login",
-        component: Login
+        path: "/login",
+        name: "Login",
+        component: Login,
     },
     {
-        path:"/register",
-        name:"Register",
-        component: Register
+        path: "/register",
+        name: "Register",
+        component: Register,
     },
-
 ];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
 });
 
 export default router;
