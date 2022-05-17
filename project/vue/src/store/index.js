f7d1e5913106f8610dd580b168e6c85eb74c826f
DevelopJKong <ybaram40@gmail.com>
2022-05-17 13:44:14 +0900

    AuthController 제작중 & [1:06:33 coding...]

--- project/vue/src/store/index.js
+++ project/vue/src/store/index.js
@@ -3,14 +3,41 @@ import { createStore } from "vuex";
 const store = createStore({
     state: {
         user: {
-            data: {name: 'Zura'},
-            token: null
-        }
+            data: {},
+            token: sessionStorage.getItem('TOKEN'),
+        },
     },
     getters: {},
-    actions: {},
-    mutations: {},
-    modules: {}
+    actions: {
+        register({ commit }, user) {
+            return fetch(`http://localhost:8000/api/register`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                method: "POST",
+                body: JSON.stringify(user),
+            })
+                .then((res) => res.json())
+                .then((res) => {
+                    console.log(res);
+                    commit("setUser", res);
+                    return res;
+                });
+        },
+    },
+    mutations: {
+        logout: (state) => {
+            state.user.data = {};
+            state.user.token = null;
+        },
+        setUser: (state,userData) => {
+            state.user.token = userData.token;
+            state.user.data = userData.user;
+            sessionStorage.setItem('TOKEN',userData.token);
+        }
+    },
+    modules: {},
 });
 
 export default store;
---
+++ project/vue/src/views/Survey.vue
@@ -0,0 +1,9 @@
+<template>
+  <PageComponent title="Survey">Content goes here</PageComponent>
+</template>
+
+<script>
+import PageComponent from "../components/PageComponents.vue";
+</script>
+
+<style scoped></style>
